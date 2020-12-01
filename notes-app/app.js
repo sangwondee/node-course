1ff2869f7061745083601c90496546488d070183
sangwondee <36039532+sangwondee@users.noreply.github.com>
2020-12-01 19:40:33 +0700

    Add files via upload

---
+++ notes-app/app.js
@@ -0,0 +1,18 @@
+// test up to git
+const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'My name is Wichan')
+
+
+// Challeage Append a message to notes to file
+// 1. Use fs.appendFileSync to append to the file
+// 2. Run the script
+// 3. Check your work by opening the file and viewing the appended text
+
+
+try {
+    fs.appendFileSync('notes.txt', ' I add text to File')
+    console.log('The "data to append" was appended to file!')
+} catch (error) {
+
+}
