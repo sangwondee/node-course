1800814b8c92e7c314d87ac80e961c9f50e98246
sangwondee <sangwondee@gmail.com>
2020-12-04 07:49:32 +0700

    test use function require and module.exports

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,33 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-// fs.writeFileSync('notes.txt', 'My name is Wichan')
+// // fs.writeFileSync('notes.txt', 'My name is Wichan')
+
+// // Challeage Append a message to notes to file
+// // 1. Use fs.appendFileSync to append to the file
+// // 2. Run the script
+// // 3. Check your work by opening the file and viewing the appended text
+
+// try {
+//     fs.appendFileSync('notes.txt', ' I add text to File')
+//     console.log('The "data to append" was appended to file!')
+// } catch (error) {
+
+// }
 
 
-// Challeage Append a message to notes to file
-// 1. Use fs.appendFileSync to append to the file
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text
+// *** อธิบายเพิ่มเติม ***
 
 
-try {
-    fs.appendFileSync('notes.txt', ' I add text to File')
-    console.log('The "data to append" was appended to file!')
-} catch (error) {
 
-}
+// const name = require('./utils');
+// const name = require()
+// console.log(name)
+
+
+const add = require('./utils')
+
+const sum = add(4, -2)
+
+console.log(sum)
+
+
---
+++ notes-app/utils.js
@@ -0,0 +1,8 @@
+const name = 'wichan'
+
+const add = function(a, b) {
+    return a + b
+}
+
+module.exports = add
+// module.exports = name
