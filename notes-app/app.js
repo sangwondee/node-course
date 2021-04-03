532029e1e286b38c44e2e3c3c2ed28330093bc8e
wichans <wichan@investree.co.th>
2021-04-03 23:54:23 +0700

    Printing in color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,12 @@
-const validator = require('validator'); // ดู parametor ได้ทีนี้ https://www.npmjs.com/package/validator
-const getNotes = require('./notes.js'); // ถ้าหาก export node ออกมาเป็น function ต้องเอาตัวแปล function มารองรับด้วย
+// const validator = require('validator'); // ดู parametor ได้ทีนี้ https://www.npmjs.com/package/validator
+// const getNotes = require('./notes.js'); // ถ้าหาก export node ออกมาเป็น function ต้องเอาตัวแปล function มารองรับด้วย
+const chalk = require('chalk');
 
-const msg = getNotes();
-console.log(msg);
+// const msg = getNotes();
+// console.log(msg);
 // console.log(validator.isEmail('wichan@emai.com'));
 // console.log(validator.isURL('www.google.com'));
 
+const greenMsg = chalk.green.bold.inverse("Success !");
 
+console.log(greenMsg);
