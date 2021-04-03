60ec2d62c6dec12016cdd33c3dfcdbaae05c9788
wichans <wichan@investree.co.th>
2021-04-03 17:49:12 +0700

    Importing npm Modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,9 @@
-// ถ้าหาก export node ออกมาเป็น function ต้องเอาตัวแปล function มารองรับด้วย
-const getNotes = require('./notes.js');
+const validator = require('validator'); // ดู parametor ได้ทีนี้ https://www.npmjs.com/package/validator
+const getNotes = require('./notes.js'); // ถ้าหาก export node ออกมาเป็น function ต้องเอาตัวแปล function มารองรับด้วย
+
 const msg = getNotes();
 console.log(msg);
+// console.log(validator.isEmail('wichan@emai.com'));
+// console.log(validator.isURL('www.google.com'));
+
+
