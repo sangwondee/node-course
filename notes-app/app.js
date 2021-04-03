43f82930bc4616b0dcfb71d8355d204750083239
wichans <wichan@investree.co.th>
2021-04-03 17:01:40 +0700

    Importing Your Own Files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,30 +1,4 @@
-// const fs = require('fs');
-
-// // fs.writeFileSync('notes.txt', 'My name is Wichan')
-
-// // Challeage Append a message to notes to file
-// // 1. Use fs.appendFileSync to append to the file
-// // 2. Run the script
-// // 3. Check your work by opening the file and viewing the appended text
-
-// try {
-//     fs.appendFileSync('notes.txt', ' I add text to File')
-//     console.log('The "data to append" was appended to file!')
-// } catch (error) {
-
-// }
-
-
-// *** อธิบายเพิ่มเติม ***
-
-
-
-// const name = require('./utils');
-// const name = require()
-// console.log(name)
-
-const add = require('./utils')
-
-const sum = add(4, -2)
-
-console.log(sum)
+// ถ้าหาก export node ออกมาเป็น function ต้องเอาตัวแปล function มารองรับด้วย
+const getNotes = require('./notes.js');
+const msg = getNotes();
+console.log(msg);
--- notes-app/utils.js
+++
@@ -1,8 +0,0 @@
-const name = 'wichan'
-
-const add = function(a, b) {
-    return a + b
-}
-
-module.exports = add
-// module.exports = name
