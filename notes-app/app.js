caa5bd7a13d1c299380fe45e3b2fb29b473ea140
wichans <wichan@investree.co.th>
2021-04-04 00:22:37 +0700

    Global npm Modules and nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,11 @@
 // const validator = require('validator'); // ดู parametor ได้ทีนี้ https://www.npmjs.com/package/validator
-// const getNotes = require('./notes.js'); // ถ้าหาก export node ออกมาเป็น function ต้องเอาตัวแปล function มารองรับด้วย
+const getNotes = require('./notes.js'); // ถ้าหาก export node ออกมาเป็น function ต้องเอาตัวแปล function มารองรับด้วย
 const chalk = require('chalk');
 
-// const msg = getNotes();
-// console.log(msg);
+const msg = getNotes();
+console.log(msg);
 // console.log(validator.isEmail('wichan@emai.com'));
 // console.log(validator.isURL('www.google.com'));
 
-const greenMsg = chalk.green.bold.inverse("Success !");
-
+const greenMsg = chalk.blue.inverse.bold("Error!");
 console.log(greenMsg);
