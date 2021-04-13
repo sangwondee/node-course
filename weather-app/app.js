a96fe8b6f573057081b8f624ad87b11227d00b9e
wichans <wichan@investree.co.th>
2021-04-13 18:59:30 +0700

    Asynchronous Basics

---
+++ weather-app/app.js
@@ -0,0 +1,14 @@
+// this is a
+
+console.log('Staing');
+
+setTimeout(() => {
+    console.log('2 second Time');
+}, 2000);
+
+setTimeout(() => {
+    console.log('0 second Time');
+})
+
+console.log('ending');
+
