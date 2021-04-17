896b6eea1bfd3a1e40de5bbd59ccdccc327e0ba0
wichans <wichan@investree.co.th>
2021-04-17 17:23:43 +0700

    Callback Abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const geocode = require('./utils/geocode')
 
 // const url = 'http://api.weatherstack.com/current?access_key=bb981d53439168c1c872dab82a496fcf&query=37.8267,-122&units=f';
 
@@ -15,16 +16,7 @@ const request = require('request');
 //     }
 // });
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1Ijoic2FuZ3dvbmRlZSIsImEiOiJja25pcDFub20wNTlqMm5wcDQ5MThqYmp0In0.GH00Svdit-esHiUaldRKWg&limit=1';
-
-request({ url:geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connet to location services!');
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.');
-    } else {
-        const latitude = response.body.features[0].center[1];
-        const longitude = response.body.features[0].center[0];
-        console.log(longitude, latitude);
-    }
-});
+geocode('Bangkok', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
+})
