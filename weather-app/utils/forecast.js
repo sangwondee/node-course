20ef58cd2994b4620d3043c257b38d49b2260b51
wichans <wichan@investree.co.th>
2021-04-18 18:10:39 +0700

    Destructuring and Property Shorthand Challenge

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -4,15 +4,15 @@ const forecast = (latitude, longitude, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=bb981d53439168c1c872dab82a496fcf&query=' + latitude  + ',' + longitude + '&units=f';
 
-    request({ url: url, json: true}, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             console.log('Unable to find location', undefined);
         } else {
-            const temperature = response.body.current.temperature;
-            const feelslike = response.body.current.feelslike;
-            const weather_descriptions = response.body.current.weather_descriptions[0];
+            const temperature = body.current.temperature;
+            const feelslike = body.current.feelslike;
+            const weather_descriptions = body.current.weather_descriptions[0];
             callback(undefined, `${weather_descriptions} It is currently ${temperature} degress. It feels like ${feelslike} degress out.`);
         }
     })
