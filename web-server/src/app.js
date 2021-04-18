07216afd524cec0cf636502e17095ea68a1c2981
wichans <wichan@investree.co.th>
2021-04-19 00:26:44 +0700

    Hello Express!

---
+++ web-server/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+
+app.get('', (req, res) => {
+    res.send('Hello Express !')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help page test');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
+app.get('/weather', (req, res) => {
+    res.send('Your Weather');
+});
+
+// port 3000 is develop port
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
