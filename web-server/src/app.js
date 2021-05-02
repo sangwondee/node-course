fe92bdce0d20cda3213226d77cd869598631238b
wichans <wichans@investree.co.th>
2021-05-02 12:31:28 +0700

    Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,28 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public/')
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title : 'Weather',
+        name : 'Wichan'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About me",
+        name: 'Wichan'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cumque quod aliquid placeat porro officiis tempore. Beatae libero vel eos dolorem aliquam minus odio quisquam, consectetur sapiente provident in atque!'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send([{
