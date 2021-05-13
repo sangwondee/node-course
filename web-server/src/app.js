fc25d00b1607e2f67a4a9e00815508b0d1bfa1d0
wichans <wichans@investree.co.th>
2021-05-13 19:35:32 +0700

    Setup app for Heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+// process.env.PORT จะใช้ port ของ heroku
+const port = process.env.PORT || 3000
 
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
@@ -99,6 +101,6 @@ app.get('*', (req, res) => {
 })
 
 // port 3000 is develop port
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port' + port)
 })
