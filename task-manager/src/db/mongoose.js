e844260e44a6df6fe87d9dbd13ca670dcf787bea
wichans <wichans@investree.co.th>
2021-07-29 20:26:11 +0700

    Data Validation and Sanitization: Part 1,2

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,45 +1,83 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
-
-// const me = new User({
-//     name: 'Wichan',
-//     age: 'sangwondee'
-// })
-
-// me.save().then((me) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error', error)
-// })
-
 // Mongoose มันจะแปลงชื่อ Model จากตัวใหญ่เป็นตัวเล็กแล้วเติม s ให้อัตโนมัติ
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
     },
-    completed: {
-        type: Boolean
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Does not contain password for set password')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
     }
 })
 
-const task = new Task({
-    description: 'lorem',
-    completed: true
+// const me = new User({
+//     name: '  Wichan  ',
+//     email: '  rightEmail@mail.com  ',
+//     password: 'newCode'
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error !', error)
+// })
+
+
+// กำหนดโครงสร้างข้อมูล structure
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
+// สร้าง model ข้อมูล
+const task = new Task({
+    description: '  Eat lunch'
+})
+
+// save ข้อมูล
 task.save().then(() => {
     console.log(task);
 }).catch((error) => {
