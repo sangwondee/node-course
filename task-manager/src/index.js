2f7f22fe28cc06678a722ef0e5963780b5ae6c2b
wichans <wichans@investree.co.th>
2021-08-11 21:09:29 +0700

    Integrating Async/Await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,74 +9,84 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res)  => {
+app.post('/users', async (req, res)  => {
+
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
+
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find({})
         res.status(200).send(users)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
 
     const _id = req.params.id
 
-    User.findById(_id).then((user) => {
+    try {
+        const user = await User.findById(_id)
 
         if (!user) {
-            return res.status(404).send()
+            return res.status(404).send();
         }
 
         res.send(user)
 
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send(e)
-    })
-})
- 
-app.post('/tasks', (req, res) => {
+    }
 
+})
+
+
+// Tasks
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
+    try {
+        await task.save()
         res.status(201).send(task)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
-app.get('/tasks/:id', (req, res) => {
-
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
 
-    Task.findById(_id).then((task) => {
-        // จะต้องเช็คแปปนี้เพราะ node มันจะคืนค่า status ออกมาเป็น success เสมอ
+    try {
+        const task = await Task.findById(_id)
         if (!task) {
             return res.status(404).send()
         }
 
         res.send(task)
-    }).catch((e) => {
+
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
 app.listen(port, () => {
