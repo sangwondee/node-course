3bf32f18f78e07d923b0fdcbf7a74532ddf67222
sangwondee <sangwondee@gmail.com>
2021-08-31 08:50:21 +0700

    Cascade Delete Tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,13 +14,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
-
-const User = require('./models/user')
-
-const main = async () => {
-    const user = await User.findById('612c5b18a2b5100a3377e305')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()
