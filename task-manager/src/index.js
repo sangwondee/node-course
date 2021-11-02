9b55a9103750eee83b07589e2dd60344bae068f1
sangwondee <sangwondee@gmail.com>
2021-11-02 16:18:16 +0700

    Validating File Uploads

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,7 +9,19 @@ const port = process.env.PORT || 3000
 const multer = require('multer');
 
 const upload = multer({
-    dest: 'images'
+    // destination folder
+    dest: 'images',
+    limits: {
+        fileSize: 1000000, // หน่วยเป็น byte
+    },
+    fileFilter(req, file, cb) {
+        // cb ย่อมากจาก callback
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 app.post('/uploads', upload.single('upload'), (req, res) => {
