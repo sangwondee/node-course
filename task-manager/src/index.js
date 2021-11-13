e4d038a2e1139e345157463108317826ea7b9ace
sangwondee <sangwondee@gmail.com>
2021-11-13 10:02:49 +0700

    Auto-Cropping and Image Formatting

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,35 +6,10 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer');
-
-const upload = multer({
-    // destination folder
-    dest: 'images',
-    limits: {
-        fileSize: 1000000, // หน่วยเป็น byte
-    },
-    fileFilter(req, file, cb) {
-        // cb ย่อมากจาก callback
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a Word document'))
-        }
-
-        cb(undefined, true)
-    }
-})
-
-app.post('/uploads', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-// Show port
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const sharp = require('sharp')
 const multer = require('multer')
 const router = new express.Router()
 
@@ -99,7 +100,10 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    // แปลงไฟล์โดยใช้ libery sharp (.png หมายถึงแปลไฟล์นั้นเข้ามาเป็นนามสกุล .png)
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -122,7 +126,6 @@ router.get('/users/:id/avatar', async(req, res) => {
 
         res.set('Content-Type', 'image/jpg')
         res.send(user.avatar)
-
     } catch (e) {
         res.send(404).send()
     }
