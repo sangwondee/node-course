bf64074146f303803b0a6ae94807b03093fa5825
sangwondee <sangwondee@gmail.com>
2021-12-22 11:26:49 +0700

    Writing Tests and Assertions

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,13 +1,17 @@
-test('Hello world !', () => {
+const { calculateTip } = require('../src/math')
 
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
 })
 
-test('This should fail', () => {
-    throw new Error('Failure')
+test('Should calculate total with default tip', () => {
+    const total = calculateTip(10);
+    expect(total).toBe(12.5)
 })
 
 // Why test
-
+//
 // - Save time
 // - Creates reliable software
 // - Gives flexibility to developers
