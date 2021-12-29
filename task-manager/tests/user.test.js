9894d0bd39e11ab62ef652191a12a85b3c3733d3
sangwondee <sangwondee@gmail.com>
2021-12-29 11:04:28 +0700

    Mocking Libraries and Wrapping up User Tests

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -5,7 +5,6 @@ const app = require('../src/app')
 const User = require('../src/models/user')
 
 const userOneId = new mongoose.Types.ObjectId()
-
 const userOne = {
     _id: userOneId,
     name: 'Mike',
@@ -94,4 +93,41 @@ test('Should not delete account for unauthenticated user', async () => {
         .delete('/users/me')
         .send()
         .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .send({
+            name: 'Jess'
+        })
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+
+    expect(user.name).toEqual('Jess')
+})
+
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .send({
+            location: 'Thailand'
+        })
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .expect(400)
 })
